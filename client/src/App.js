e8640f7f941ddb55dd63ba1210b47b800f10e95e
Ian Goodwin <iangoodwin173@gmail.com>
2023-07-11 18:52:37 -0400

    added navbar and react router components along with components folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import About from './components/About';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar></Navbar>
+      <Switch>
+        <Route exact path='/' component={Home}></Route>
+        <Route path='/About' component={About}></Route>
+        <Route path='/Signup' component={Signup}></Route>
+        <Route path='/Login' component={Login}></Route>
+        <Route path='/Contact' component={Contact}></Route>
+      </Switch>
+    </Router>
+      
   );
 }
 
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/About'>About</Link>
+                </li>
+                <li>
+                    <Link to='/Login'>Login</Link>
+                </li>
+                <li>
+                    <Link to='/Signup'>Signup</Link>
+                </li>
+                <li>
+                    <Link to='/'>Contact</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
