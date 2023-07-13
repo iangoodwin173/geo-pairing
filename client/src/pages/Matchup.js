a88be58c0d2478958f45e2a1b68b2e8ec4d9bb31
Carol Wargo <Carolwargo@gmail.com>
2023-07-13 12:17:38 -0400

    added pages with basic code

---
+++ client/src/pages/Matchup.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Matchup() {
+    return (
+        <div>
+            <h1>Matchup Page Route</h1>
+        </div>
+    )
+}
---
+++ client/src/pages/Profile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Profile() {
+    return (
+        <div>
+            <h1>Profile Page Route</h1>
+        </div>
+    )
+}
