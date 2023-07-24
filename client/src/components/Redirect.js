b3c8fbbc0ff2f9af32c018c4f98efd1729e122cc
Carol Wargo <Carolwargo@gmail.com>
2023-07-24 09:35:37 -0400

    update file names

--- client/src/components/Redirect.js
+++ client/src/components/Redirect.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Import Link from React Router
-import './redirect.css'
+import '.'
 
 export default function Redirect() {
   return (
