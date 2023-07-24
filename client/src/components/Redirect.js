78b96f10e3a22d6302ba548977fe7c85d7be3fbe
Carol Wargo <Carolwargo@gmail.com>
2023-07-24 09:33:42 -0400

    package lock update and redirect.js styling

--- client/src/components/Redirect.js
+++ client/src/components/Redirect.js
@@ -1,20 +1,22 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom'; // Import Link from React Router
+import './redirect.css'
 
 export default function Redirect() {
-    
-    return (
-        <div className='redirect-text'>
-            <p>Successful Sign-Up! 
-                <br></br>
-                Please log in with your 
-                <br></br>
-                new credentials to continue
-                <br></br>
-                using this site.
-             </p>
-            
-   
-        </div>
-    )
+  return (
+    <div className='redirect-container'>
+      <div className='redirect-text'>
+        <p>Successful Sign-Up! 
+          <br></br>
+          Please log in with your 
+          <br></br>
+          new credentials to continue
+          <br></br>
+          using this site.
+        </p>
+        
+        <Link to="/login">Go to Login</Link> {/* Use Link to navigate to the login page */}
+      </div>
+    </div>
+  );
 }
