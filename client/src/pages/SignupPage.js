bc1c1f2107e5f6562a05b3aa0a63f662aa7e45a5
Ian <122403641+iangoodwin173@users.noreply.github.com>
2023-07-24 23:18:14 -0400

    Miccibranch (#18)
    * finalizedforms
    
    * updatedforms
    
    * uodate
    
    * dash
    
    * savedtheday
    
    * Commit changes in SignupPage.js before switching to main branch
    
    * killin it
    
    ---------
    
    Co-authored-by: Micci11 <miccibenner@gmail.com>

--- client/src/pages/SignupPage.js
+++ client/src/pages/SignupPage.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { useMutation, gql } from '@apollo/client';
 import { useForm } from 'react-hook-form';
+
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
 
 const SIGNUP_MUTATION = gql`
   mutation Mutation($username: String!, $email: String!, $password: String!) {
@@ -47,39 +47,53 @@ const SignupPage = () => {
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error occurred during signup :(</p>;
-
   return (
     <Container>
       <Row className="d-flex align-items-center justify-content-center vh-100">
-        <Col xs={12} sm={6} className="text-center">
+        <Col lg={12} sm={6} className="text-center">
           <Form id="signupForm" onSubmit={handleSubmit(onSubmit)}>
-            <Form.Control
-              {...register('email', { required: "Email is required." })}
-              type="email"
-              placeholder="Enter email"
-            />
-            {errors.email && <p>{errors.email.message}</p>}
+            <Form.Group controlId="email" className="mb-1">
+              <Form.Label></Form.Label>
+              <Form.Control
+                {...register('email', { required: "Email is required." })}
+                type="email"
+                placeholder="Enter email"
+              />
+              {errors.email && <p>{errors.email.message}</p>}
+              <Form.Text className="text-muted">
+               
+              </Form.Text>
+            </Form.Group>
 
-            <Form.Control
-              {...register('fullName', { required: "Full name is required." })}
-              type="text"
-              placeholder="Full Name"
-            />
-            {errors.fullName && <p>{errors.fullName.message}</p>}
+            <Form.Group controlId="fullName" className="mb-1">
+              <Form.Label></Form.Label>
+              <Form.Control
+                {...register('fullName', { required: "Full name is required." })}
+                type="text"
+                placeholder="Full Name"
+              />
+              {errors.fullName && <p>{errors.fullName.message}</p>}
+            </Form.Group>
 
-            <Form.Control
-              {...register('username', { required: "Username is required." })}
-              type="text"
-              placeholder="Username"
-            />
-            {errors.username && <p>{errors.username.message}</p>}
+            <Form.Group controlId="username" className="mb-1">
+              <Form.Label></Form.Label>
+              <Form.Control
+                {...register('username', { required: "Username is required." })}
+                type="text"
+                placeholder="Username"
+              />
+              {errors.username && <p>{errors.username.message}</p>}
+            </Form.Group>
 
-            <Form.Control
-              {...register('password', { required: "Password is required." })}
-              type="password"
-              placeholder="Password"
-            />
-            {errors.password && <p>{errors.password.message}</p>}
+            <Form.Group controlId="password" className="mb-4">
+              <Form.Label></Form.Label>
+              <Form.Control
+                {...register('password', { required: "Password is required." })}
+                type="password"
+                placeholder="Password"
+              />
+              {errors.password && <p>{errors.password.message}</p>}
+            </Form.Group>
 
             <Button variant="primary" type="submit" style={{ width: '100%' }}>
               Sign Up
